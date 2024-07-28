b8079ba2ad70d5dc60cad5e8208bd774e7eeae5b
Mohamedzake <zakem7671@gmail.com>
2024-07-28 13:02:15 +0300

    j

---
+++ src/services/supabase.js
@@ -0,0 +1,15 @@
+// import { createClient } from "@supabase/supabase-js";
+
+// export const supabaseUrl = "https://dclaevazetcjjkrzczpc.supabase.co";
+// const supabaseKey =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjbGFldmF6ZXRjamprcnpjenBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyOTIzNDQsImV4cCI6MTk5ODg2ODM0NH0.LGg0M-taoHgKtxCzr9owrb09epnPaO_Yfz6xVE54sIY";
+// const supabase = createClient(supabaseUrl, supabaseKey);
+
+// export default supabase;
+import { createClient } from "@supabase/supabase-js";
+export const supabaseUrl = "https://ovbwqglayexjbfkstmgg.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92YndxZ2xheWV4amJma3N0bWdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcyNDA4NTAsImV4cCI6MjAyMjgxNjg1MH0.hPnvl_oLcdOyQblE-TuKZQQ5X3ZrAToO9wxKq2Mt0gY";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
